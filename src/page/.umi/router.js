d808e0b0d8a1476e681efc6cf9479097f9f923a3
chijiao323 <29945440@qq.com>
2019-06-23 13:39:54 +0800

    NO.1

---
+++ src/page/.umi/router.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@tmp/history';
+
+
+const Router = DefaultRouter;
+
+const routes = [
+  {
+    "path": "/",
+    "component": require('../HelloWorld').default,
+    "exact": true
+  },
+  {
+    "component": () => React.createElement(require('C:/Users/13509441738/Desktop/antd-621/node_modules/_umi-build-dev@1.10.3@umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/page', hasRoutesInConfig: true })
+  }
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+history.listen(routeChangeHandler);
+routeChangeHandler(history.location);
+
+export { routes };
+
+export default function RouterWrapper(props = {}) {
+  return (
+<Router history={history}>
+      { renderRoutes(routes, props) }
+    </Router>
+  );
+}
